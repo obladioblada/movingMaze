3549009013055dd937ea213b6b63d7a49c1f161b
basilg01 <basilg01@stepstone.be>
2021-05-02 21:28:12 +0200

    started implementing swapping, todo: clean code and create swapping method

--- Assets/scripts/grid/GridManager.cs
+++ Assets/scripts/grid/GridManager.cs
@@ -1,107 +1,130 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
-namespace grid
-{
-    public class GridManager : MonoBehaviour
-    {
-        public const int ROWS = 7;
-        public const int COLUMNS = 7;
+namespace grid {
+    public class GridManager : MonoBehaviour {
+        private const int ROWS = 7;
+        private const int COLUMNS = 7;
         [SerializeField] private GameObject tile;
         [SerializeField] private GameObject allowMoving;
         private List<Tile> _tiles;
+        private Tile spare;
 
-        enum Axis
-        {
+        private enum Axis {
             X,
             Y
         }
 
-        void Awake()
-        {
+        void Awake() {
             _tiles = new List<Tile>();
+            tile.transform.parent = transform;
+            spare = new Tile(-1, tile, GetRandomTilePath());
+            _tiles.Add(spare);
             GenerateGrid();
         }
 
         // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetKeyDown("left"))
-            {
-                Shift(tile, 0, Axis.X, Vector3.left);
+        void Update() {
+            if (Input.GetKeyDown("left")) {
+                Shift(1, Axis.X, Vector3.left);
             }
 
-            if (Input.GetKeyDown("right"))
-            {
-                Shift(tile, 0, Axis.X, Vector3.right);
+            if (Input.GetKeyDown("right")) {
+                Shift(1, Axis.X, Vector3.right);
             }
 
-            if (Input.GetKeyDown("up"))
-            {
-                Shift(tile, 0, Axis.Y, Vector3.up);
+            if (Input.GetKeyDown("up")) {
+                Shift(1, Axis.Y, Vector3.up);
             }
-
-            if (Input.GetKeyDown("down"))
-            {
-                Shift(tile, 0, Axis.Y, Vector3.down);
+            if (Input.GetKeyDown("down")) {
+                Shift(1, Axis.Y, Vector3.down);
+            }
+            if (Input.GetKeyDown("r")) {
+               spare.gameObject.transform.Rotate(0,0, 90);
             }
         }
 
-        private void GenerateGrid()
-        {
-            for (var x = 0; x < COLUMNS; x++)
-            {
-                if (x % 2 == 1)
-                {
+         void GenerateGrid() {
+            for (var x = 0; x < COLUMNS; x++) {
+                if (x % 2 == 1) {
                     Instantiate(allowMoving, transform).transform
-                        .position = new Vector3(x, -1, 0);
+                        .position = new Vector3(x, -1, 1);
                     Instantiate(allowMoving, transform).transform
-                        .position = new Vector3(x, ROWS, 0);
+                        .position = new Vector3(x, ROWS, 1);
                 }
 
-                for (var y = 0; y < ROWS; y++)
-                {
-                    if (y % 2 == 1 && (x == 0 || x == COLUMNS - 1))
-                    {
-                        Instantiate(allowMoving, transform).transform
-                            .position = new Vector3(-1, y, 0);
-                        Instantiate(allowMoving, transform).transform
-                            .position = new Vector3(COLUMNS, y, 0);
+                for (var y = 0; y < ROWS; y++) {
+                    GameObject go = SpawnTile(x, y);
+                    var tilePath = GetRandomTilePath();
+                    if ((x == 0 || x == COLUMNS - 1) && (y == 0 || y == ROWS - 1)) {
+                        tilePath = "Tiles/tile_3_blue";
                     }
 
-                    _tiles.Add(new Tile(x + y * 10, SpawnAndStore(x, y)));
+                    if (y % 2 == 1 && (x == 0 || x == COLUMNS - 1)) {
+                        Instantiate(allowMoving, transform).transform
+                            .position = new Vector3(-1, y, 1);
+                        Instantiate(allowMoving, transform).transform
+                            .position = new Vector3(COLUMNS, y, 1);
+                    }
+
+                    _tiles.Add(new Tile(x + y * 10, go, tilePath));
                 }
             }
         }
 
-        void Shift(GameObject ta, int index, Axis axis, Vector3 direction)
-        {
+        private static string GetRandomTilePath() {
+            return "Tiles/tile_" + Random.Range(1, 4);
+        }
+
+        void Shift(int index, Axis axis, Vector3 direction) {
             if (index % 2 != 1) return;
-            if (axis == Axis.X)
-            {
-                foreach (var t in _tiles.Where(x => (int) x.gameObject.transform.position.y == index))
-                {
+            if (axis == Axis.X) {
+                if (direction == Vector3.right) {
+                    var leftOver = _tiles.First(t => t.gameObject.transform.position == new Vector3(COLUMNS - 1, index ));
+                    leftOver.gameObject.transform.position = spare.gameObject.transform.position;
+                    spare.gameObject.transform.position = new Vector2(-1, index);
+                    spare = leftOver;
+                }
+                else if (direction  == Vector3.left){
+                    var leftOver = _tiles.Find(t => t.gameObject.transform.position == new Vector3(0, index));
+                    leftOver.gameObject.transform.position = spare.gameObject.transform.position;
+                    spare.gameObject.transform.position = new Vector2(COLUMNS, index);
+                    spare = leftOver;
+                }
+
+                foreach (var t in _tiles.Where(x => (int) x.gameObject.transform.position.y == index)) {
                     t.Shift(direction);
                 }
-            } else
-            {
-                foreach (var t in _tiles.Where(x => (int) x.gameObject.transform.position.x == index))
-                {
+            } else {
+                if (direction == Vector3.up) {
+                    var leftOver = _tiles.Find(t => t.gameObject.transform.position == new Vector3(index, ROWS - 1));
+                    leftOver.gameObject.transform.position = spare.gameObject.transform.position;
+                    spare.gameObject.transform.position = new Vector2(index, -1);
+                    spare = leftOver;
+                }
+                else if (direction == Vector3.down) {
+                    var leftOver = _tiles.Find(t => t.gameObject.transform.position == new Vector3(index, 0));
+                    leftOver.gameObject.transform.position = spare.gameObject.transform.position;
+                    spare.gameObject.transform.position = new Vector2(index, ROWS);
+                }
+
+                foreach (var t in _tiles.Where(x => (int) x.gameObject.transform.position.x == index)) {
                     t.Shift(direction);
                 }
             }
         }
 
-        private GameObject SpawnAndStore(int x, int y)
-        {
+        private GameObject SpawnTile(int x, int y) {
             var go = Instantiate(
                 tile,
                 gameObject.transform.position,
                 Quaternion.Euler(0, 0, Random.Range(0, 4) * 90),
                 transform);
-            go.transform.position = new Vector3(x, y, 5);
+            go.transform.position = new Vector3(x, y, 0);
             go.name = y + "," + x;
             return go;
         }
--- Assets/scripts/grid/Tile.cs
+++ Assets/scripts/grid/Tile.cs
@@ -9,19 +9,26 @@ namespace grid
         public int _id;
         public GameObject gameObject;
 
-        public Tile(int id, GameObject go)
+        public Tile(int id, GameObject gameObject) {
+            _id = id;
+            this.gameObject = gameObject;
+        }
+
+        public Tile(int id, GameObject go, String tilePath)
         {
             _id = id;
             gameObject = go;
-            //Set the Texture you assign in the Inspector as the main texture (Or Albedo)
-            gameObject.GetComponent<SpriteRenderer>().sprite =
-                Resources.Load<Sprite>("Tiles/tile_" + Random.Range(1, 4));
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(tilePath);
         }
 
 
         public void Shift(Vector3 direction)
         {
             gameObject.transform.position += direction;
+        }
+
+        public Tile Clone() {
+            return new Tile(_id, gameObject);
         }
     }
 }
