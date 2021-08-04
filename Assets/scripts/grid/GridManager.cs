486db5895201e07731513405cb50be11f0d70f02
basilg01 <basilg01@stepstone.be>
2021-08-04 21:11:26 +0200

    adjusted arrows position

--- Assets/scripts/grid/GridManager.cs
+++ Assets/scripts/grid/GridManager.cs
@@ -151,9 +151,9 @@ namespace grid {
                 // creating arrows bottom/top side
                 if (x % 2 == 1) {
                     _arrows[N + x - 2] = new Arrow(Instantiate(allowMoving, transform), ShiftAxis.Vertically,
-                        new Vector3(x, -1, z), Vector3.up, x);
+                        new Vector3(x, -0.5f, z), Vector3.up, x);
                     _arrows[N + x - 1] = new Arrow(Instantiate(allowMoving, transform), ShiftAxis.Vertically,
-                        new Vector3(x, N, z), Vector3.down, x);
+                        new Vector3(x, N-0.5f, z), Vector3.down, x);
                 }
                 for (var y = 0; y < N; y++) {
                     // create tiles player base
@@ -199,9 +199,9 @@ namespace grid {
                     // creating arrows right/left side
                     if (y % 2 == 1 && x == 0) {
                         _arrows[y - 1] = (new Arrow(Instantiate(allowMoving, transform), ShiftAxis.Horizontally,
-                            new Vector3(-1, y, z), Vector3.right, y));
+                            new Vector3(-0.5f, y, z), Vector3.right, y));
                         _arrows[y] = (new Arrow(Instantiate(allowMoving, transform), ShiftAxis.Horizontally,
-                            new Vector3(N, y,z), Vector3.left, y));
+                            new Vector3(N-0.5f, y,z), Vector3.left, y));
                     }
                 }
             }
